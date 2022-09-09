74b40c96069b7d5387ce5f964979b3e1fbb50597
Juliette Pretot <julsh@google.com>
2022-09-09 10:44:52 +0000

    init

---
+++ src/main.rs
@@ -0,0 +1,17 @@
+#![no_std]
+#![no_main]
+
+extern crate wee_alloc;
+
+#[global_allocator]
+static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+
+#[panic_handler]
+fn panic(_info: &core::panic::PanicInfo) -> ! {
+    loop {}
+}
+
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    loop {}
+}
